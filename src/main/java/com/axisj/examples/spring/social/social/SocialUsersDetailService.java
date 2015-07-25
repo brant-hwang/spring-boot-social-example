deecb4b64fbee83bf569de7fb38d9ac61f187639
Brant Hwang <dlstj3039@gmail.com>
2015-07-25 22:02:48 +0900

    Test Code 정리~

--- src/main/java/com/axisj/examples/spring/social/social/SocialUsersDetailService.java
+++ src/main/java/com/axisj/examples/spring/social/social/SocialUsersDetailService.java
@@ -1,16 +1,11 @@
 package com.axisj.examples.spring.social.social;
 
 import org.springframework.dao.DataAccessException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.social.security.SocialUserDetails;
 import org.springframework.social.security.SocialUserDetailsService;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class SocialUsersDetailService implements SocialUserDetailsService {
 
@@ -23,10 +18,6 @@ public class SocialUsersDetailService implements SocialUserDetailsService {
 	@Override
 	public SocialUserDetails loadUserByUserId(String userId) throws UsernameNotFoundException, DataAccessException {
 		UserDetails userDetails = userDetailsService.loadUserByUsername(userId);
-
-		List<GrantedAuthority> grantedAuthorityList = new ArrayList<>();
-		grantedAuthorityList.add(new SimpleGrantedAuthority("ROLE_USER"));
-
 		return (FrontUserDetail) userDetails;
 	}
 }
